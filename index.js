2db19ebc7f63fe29db0f3c133aaa9c3445dbfc35
tongnoiza <72687744+tongnoiza@users.noreply.github.com>
2023-12-16 15:30:41 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 const express = require('express')
 const app = express()
+const port = process.env.PORT || 3000
+
 app.all('/', (req, res) => {
     console.log("req ",req.query)
     console.log("Just got a request!")
     res.send('Yo!')
 })
-app.listen(process.env.PORT || 3000)
+app.listen(port,()=>{
+      console.log("รันบน port ",port)
+})
