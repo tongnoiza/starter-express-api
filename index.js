1bcdf3b8f386c1bde07f82c7fd8ef8279bd502b7
sontaya <sontaya@aspiron.co.th>
2023-12-16 16:09:05 +0700

    iiii

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const express = require('express')
+var cors = require('cors')
 const app = express()
 const port = process.env.PORT || 443
-
+app.use(cors())
 app.all('/', (req, res) => {
   console.log("รันบน port ",port)
     console.log("Just got a request!")
